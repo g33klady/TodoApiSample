71d09ecca1742b85bb8bd27666642988230a46b7
Hilary <g33klady@gmail.com>
2018-06-03 14:47:23 -0400

    added swagger

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using TodoApi.Models;
+using NJsonSchema;
+using NSwag.AspNetCore;
+using System.Reflection;
 
 namespace TodoApi
 {
@@ -20,6 +23,29 @@ namespace TodoApi
         {
             app.UseDefaultFiles();
             app.UseStaticFiles();
+
+            app.UseSwaggerUi(typeof(Startup).GetTypeInfo().Assembly, settings =>
+            {
+                settings.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "ToDo API";
+                    document.Info.Description = "A simple ASP.NET Core web API";
+                    document.Info.TermsOfService = "None";
+                    document.Info.Contact = new NSwag.SwaggerContact
+                    {
+                        Name = "Hilary Weaver-Robb",
+                        Email = string.Empty,
+                        Url = "https://twitter.com/g33klady"
+                    };
+                    document.Info.License = new NSwag.SwaggerLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = "https://example.com/license"
+                    };
+                };
+            });
+
             app.UseMvc();
         }
     }
