7100345255adeed0cc97ecbd81dc13ca5e7e0b1d
Hilary <g33klady@gmail.com>
2018-06-10 16:17:08 -0400

    added name and date validation; added limit method but commented out

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using TodoApi.Models;
@@ -15,11 +16,11 @@ namespace TodoApi.Controllers
         {
             _context = context;
 
-            if (_context.TodoItems.Count() == 0)
-            {
-                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
-                _context.SaveChanges();
-            }
+            //if (_context.TodoItems.Count() == 0)
+            //{
+            //    _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+            //    _context.SaveChanges();
+            //}
         }
 
         [HttpGet]
@@ -27,6 +28,18 @@ namespace TodoApi.Controllers
         {
             return _context.TodoItems.ToList();
         }
+
+        //[HttpGet("limit")]
+        //public ActionResult<List<TodoItem>> GetSome(int qty)
+        //{
+        //    var fullList = _context.TodoItems.ToList();
+        //    var returnList = new List<TodoItem>();
+        //    for (int i = 0; i < qty; i++)
+        //    {
+        //        returnList.Add(fullList[i]);
+        //    }
+        //    return returnList;
+        //}
 
         [HttpGet("{id}", Name = "GetTodo")]
         public ActionResult<TodoItem> GetById(long id)
@@ -42,6 +55,14 @@ namespace TodoApi.Controllers
         [HttpPost]
         public IActionResult Create([FromBody]TodoItem item)
         {
+            if(!IsItemNameValid(item))
+            {
+                return BadRequest("Name is required and must be 1-255 chars long.");
+            }
+            if(!IsItemDateValid(item))
+            {
+                return BadRequest("Date must be valid and in the future.");
+            }
             _context.TodoItems.Add(item);
             _context.SaveChanges();
 
@@ -56,7 +77,10 @@ namespace TodoApi.Controllers
             {
                 return NotFound();
             }
-
+            if (todo.Name == "")
+            {
+                return BadRequest("Name is required.");
+            }
             todo.IsComplete = item.IsComplete;
             todo.Name = item.Name;
             todo.DateDue = item.DateDue;
@@ -78,6 +102,35 @@ namespace TodoApi.Controllers
             _context.TodoItems.Remove(todo);
             _context.SaveChanges();
             return NoContent();
+        }
+
+        public bool IsItemNameValid(TodoItem item)
+        {
+            var name = item.Name;
+            if(name == null || name.Length > 255 || name.Length < 1)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool IsItemDateValid(TodoItem item)
+        {
+           var date = item.DateDue;
+           if (date == DateTime.MinValue)
+            {
+                return true;
+            }
+           DateTime today = DateTime.Today;
+           var diff = DateTime.Compare(date, today);
+           if(diff < 0)
+            {
+                return false; //item.datedue is in the past
+            }
+           else
+            {
+                return true; //item.datedue is today or in the future
+            }
         }
     }
 }
