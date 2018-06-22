ffcad129305678f68c0e588bf5b3dcde86d4ac0b
Hilary <g33klady@gmail.com>
2018-06-22 13:37:01 -0400

    added class for tests, with testfixture and onetimesetup method

---
+++ TodoApiTests/TodoIntegrationTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoApiTests
+{
+    [TestFixture]
+    public class TodoIntegrationTests
+    {
+        private static string _baseUrl;
+
+        [OneTimeSetUp]
+        public void TestClassInitialize()
+        {
+            //make sure this has the correct port!
+            _baseUrl = "https://localhost:44350/api/Todo/";
+        }
+    }
+}
