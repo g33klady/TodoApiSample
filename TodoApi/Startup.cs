07a3906217d7f3707196c2e8063bb0f7f2e506ec
Hilary <g33klady@gmail.com>
2018-08-11 12:28:02 -0400

    clean up unused stuff

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using TodoApi.Models;
-using System.Reflection;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Swashbuckle.AspNetCore.Swagger;
 using System.Collections.Generic;
