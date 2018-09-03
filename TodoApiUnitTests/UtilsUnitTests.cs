486998914e329dcb4e838e6c53ecaa424b78ae35
Hilary <g33klady@gmail.com>
2018-09-03 16:21:56 -0400

    added two unit tests

---
+++ TodoApiUnitTests/UtilsUnitTests.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TodoApiUnitTests
+{
+    [TestClass]
+    public class UtilsUnitTests
+    {
+
+        [TestMethod]
+        public void IsItemNameValidReturnsFalseWhenItemNameIsEmptyStringx()
+        {
+            TodoApi.Models.TodoItem item = new TodoApi.Models.TodoItem();
+            item.Name = "";
+
+            TodoApi.Utils.IsItemNameValid(item);
+
+        }
+
+        [TestMethod]
+        public void IsItemNameValidReturnsFalseWhenItemNameIsEmptyString()
+        {
+            TodoApi.Models.TodoItem item = new TodoApi.Models.TodoItem();
+            item.Name = "";
+
+            var result = TodoApi.Utils.IsItemNameValid(item);
+
+            Assert.IsFalse(result);
+
+        }
+    }
+}
